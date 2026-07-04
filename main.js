5de0361550a21c54eabce39cbdc8266932752224
Sidelnikov Oleksandr <mmf2003@ukr.netr>
2026-07-04 12:17:21 +0300

    Homework finished

--- main.js
+++ main.js
@@ -7,13 +7,21 @@ console.log("Lection 30");
  * Функція повертає true, якщо електронна адреса валідна, і false в іншому випадку.
  *
  */
-
+console.log("Ex.1 Использовал ^ в [], как НЕ");
+console.log("--------------------------------------------------");
 function isValidEmail(email) {
-    // code here
-}
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-// console.log(isValidEmail('example@example.com')) // Повинно вивести: true
-// console.log(isValidEmail('invalid-email'))       // Повинно вивести: false
+    return pattern.test(email);
+}
+console.log(
+    "pattern = example@example.com -->",
+    isValidEmail("example@example.com"),
+); // Повинно вивести: true
+console.log("pattern = invalid-email -->", isValidEmail("invalid-email")); // Повинно вивести: false
+console.log("pattern = test@com -->", isValidEmail("test@com"));
+console.log("pattern = @gmail.com -->", isValidEmail("@gmail.com"));
+console.log("pattern = asda @gmail.com -->", isValidEmail("asda @gmail.com"));
 
 /*
  * #2
@@ -27,12 +35,24 @@ function isValidEmail(email) {
 
 */
 
+console.log("Ex.2 Сделал усиленную регулярка для проверки /path1/path2 и тд");
+console.log("--------------------------------------------------");
+
 function isValidUrl(url) {
-    // code here
+    const pattern = /^https?:\/\/([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[^\s]*)?$/;
+
+    return pattern.test(url);
 }
 
-// console.log(isValidUrl('https://www.example.com')) // Повинно вивести: true
-// console.log(isValidUrl('invalid-url'))             // Повинно вивести: false
+console.log(
+    "https://www.example.com -->",
+    isValidUrl("https://www.example.com"),
+); // Повинно вивести: true
+console.log("pattern = invalid-url -->", isValidUrl("invalid-url")); // Повинно вивести: false
+console.log(
+    "pattern = https://www.example.com/path1/path2 -->",
+    isValidUrl("https://www.example.com/path1/path2"),
+); // Повинно вивести: false
 
 // Експорт функції для використання та тестування
 /* export { isValidEmail, isValidUrl }; */
